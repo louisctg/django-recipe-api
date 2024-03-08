3e7df7a7a22c9d0d444d757b6db18cd816355061
Louis Thurston-Gibson <dev@louisthurstongibson.com>
2024-03-08 13:43:51 +0000

    Fix linting errors

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,4 +1,3 @@
-
 """
 Test custom Django management commands.
 """
@@ -22,7 +21,6 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         patched_check.assert_called_once_with(databases=['default'])
-
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
