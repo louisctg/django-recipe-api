934b88766c620e497d68f5e8bf53cac202246d7c
Louis Thurston-Gibson <dev@louisthurstongibson.com>
2024-03-08 14:33:11 +0000

    Fix lint errors

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,6 +4,7 @@ Tests for models.
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
     """Test models."""
 
@@ -33,16 +34,16 @@ class ModelTests(TestCase):
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email(self):
-      """Test creating user without email raises error."""
-      with self.assertRaises(ValueError):
-        get_user_model().objects.create_user('', 'sample123')
+        """Test creating user without email raises error."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'sample123')
 
     def test_create_new_superuser(self):
-      """Test creating a new superuser."""
-      user = get_user_model().objects.create_superuser(
-          'test@example.com',
-          'test123',
-      )
+        """Test creating a new superuser."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
 
-      self.assertTrue(user.is_superuser)
-      self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
